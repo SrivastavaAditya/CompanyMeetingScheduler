663c903bba1ed85f39e1dccbe2682b2c058b267c
SrivastavaAditya <aditya.srivastava@svam.com>
2019-07-20 21:54:08 +0530

    home activity UI

--- app/src/main/java/com/example/adityasrivastava/meetingscheduler/HomeActivity.kt
+++ app/src/main/java/com/example/adityasrivastava/meetingscheduler/HomeActivity.kt
@@ -1,12 +1,12 @@
 package com.example.adityasrivastava.meetingscheduler
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
